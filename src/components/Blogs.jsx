4ba1e676e335172fa2ffef4768c50238e3cd3197
aaronamano <aaroncedric2005@yahoo.com>
2026-05-04 00:15:52 -0400

    made second blog

--- src/components/Blogs.jsx
+++ src/components/Blogs.jsx
@@ -3,9 +3,13 @@ export default function Blogs() {
   const BASEURL = "/data/blogs"
 
   return (
-    <div>
+    <>
       <p>April 29, 2026</p>
       <a href={`${BASEURL}/blog1.html`} target="_blank" rel="noopener noreferrer" className="text-blue-700 text-xl">Looking back and why I chose computer science</a>
-    </div>
+
+      <p className="mt-6">May 3, 2026</p>
+      <a href={`${BASEURL}/blog2.html`} target="_blank" rel="noopener noreferrer" className="text-blue-700 text-xl">How watching Bluelock changed me</a>
+
+    </>
   );
 }
