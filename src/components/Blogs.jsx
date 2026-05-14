5ed3622a2aa8bb91a68ece4db64d841407d83b79
aaronamano <aaroncedric2005@yahoo.com>
2026-05-14 02:44:58 -0400

    published blog 4

--- src/components/Blogs.jsx
+++ src/components/Blogs.jsx
@@ -4,7 +4,9 @@ export default function Blogs() {
 
   return (
     <>
-      <p>April 29, 2026</p>
+      <p>Welcome to my blog channel where I pretty much talk about my life</p>
+
+      <p className="mt-6">April 29, 2026</p>
       <a href={`${BASEURL}/blog1.html`} target="_blank" rel="noopener noreferrer" className="text-blue-700 text-xl">Looking back and why I chose computer science</a>
 
       <p className="mt-6">May 3, 2026</p>
@@ -12,6 +14,9 @@ export default function Blogs() {
 
       <p className="mt-6">May 8, 2026</p>
       <a href={`${BASEURL}/blog3.html`} target="_blank" rel="noopener noreferrer" className="text-blue-700 text-xl">Navigating through noise</a>
+
+      <p className="mt-6">May 14, 2026</p>
+      <a href={`${BASEURL}/blog4.html`} target="_blank" rel="noopener noreferrer" className="text-blue-700 text-xl">Fullstack dev is boring</a>
 
     </>
   );
